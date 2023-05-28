641d3065d2ab2bf6a1d3590720b9b4bfe80fcc71
Cunningham Code Consultancy <104110053+cunninghamcodeconsultancy@users.noreply.github.com>
2023-05-28 14:33:03 +0100

    Create cube class

--- run.py
+++ run.py
@@ -4,7 +4,15 @@ import pygame
 import tkinter as tk
 from tkinter import messagebox
 
-
+class cube(object):
+    rows = 0
+    w = 0
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        pass
+    def move(self, dirnx, dirny):
+        pass
+    def draw(self, surface, eyes=False):
+        pass
 
 def drawGrid(w, rows, surface):
     # Figure out how big each square in the grid is gonna be
