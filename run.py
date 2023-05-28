396854858a19f1a00a90a91a326759a3ff914d28
Cunningham Code Consultancy <104110053+cunninghamcodeconsultancy@users.noreply.github.com>
2023-05-28 14:20:09 +0100

    Create draw grid function and modify main and redraw function to

--- run.py
+++ run.py
@@ -1,17 +1,33 @@
+def drawGrid(w, rows, surface):
+    # Figure out how big each square in the grid is gonna be
+    # Make sure we don't get large decimal number
+    sizeBtwn = w // rows
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+    # Draw two lines every loop of the for loop. Arguments are the start position of the line amd the end position
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x,w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
+
+
 def redrawWindow(surface):
+    global rows, width
     # Fill screen with black color
     win.fill((0, 0, 0))
-    drawGrid(surface)
+    drawGrid(width, rows, surface)
     pygame.display.update()
 
 
 # Main Func
 def main():
+    global width, rows
     width = 500
-    height = 500
     # If you want to make it harder set it to 10 there won't be enough room for the snake to move around
     rows = 20
-    win = pygame.display.set_mode((width, height))
+    # Height not necessary we are just gonna draw a square
+    win = pygame.display.set_mode((width, width))
     # Snake color and position
     s = snake((255, 0, 0), (10, 10))
     flag = True
