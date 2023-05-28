16dd21764a071cabc508547776d69dc55d8a1be1
Cunningham Code Consultancy <104110053+cunninghamcodeconsultancy@users.noreply.github.com>
2023-05-28 15:03:40 +0100

    Create snake object and start move function

--- run.py
+++ run.py
@@ -14,6 +14,68 @@ class cube(object):
     def draw(self, surface, eyes=False):
         pass
 
+class snake(object):
+    body = []
+    turns = {}
+    def __init__(self, color, pos):
+        self.color = color
+        # The head of the snake is equal to the cube at the given position
+        self.head = cube(pos)
+        # Append to the body the head
+        self.body.append(self.head)
+        # Direction for moving our snake, can only be moving in one direction at the same time
+        self.dirnx = 0
+        self.dirny = 1
+    def move(self):
+        for event in pygame.event.get()L
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            keys = pygame.key.pygame.key.get_pressed()
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    # Add a key which is the current position of the head of our snake, equal to what direction we turned
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    # Add a key which is the current position of the head of our snake, equal to what direction we turned
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    # Add a key which is the current position of the head of our snake, equal to what direction we turned
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_DOWN]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    # Add a key which is the current position of the head of our snake, equal to what direction we turned
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        # I for index and c for cube. Get the cube object in self.body
+        for i, c in enumerate(self.body):
+                # Grab the cube object position and see if that position is in the turn list
+                p = c.pos[:]
+                if p in self.turns:
+                    # The turn where snake will be moving is equal to the turns list at that index
+                    turn = self.turns[p]
+                    # Move in direction x and direction y
+                    c.move(turn[0],turn[1])
+                    # If snake is on the last cube remove the turn
+                    if i == len(self.body)-1:
+                        self.turns.pop(p)
+                else:
+                    # Checking whether or not we've reached the edge of the screen
+                    # If we're moving left and the exposition of the cub is less than or equal to 0, change the position so that is goes to the right side of the screen
+                    if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                    elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0, c.pos[1])
+                    elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                    elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
+                    else: c.move(c.dirnx,c.dirny)
+    
 def drawGrid(w, rows, surface):
     # Figure out how big each square in the grid is gonna be
     # Make sure we don't get large decimal number
